c6ed0b31728a049da65ba5027bdbdad93d62fc40
Rui Yang <ryang@pivotal.io>
2020-05-05 23:30:43 -0400

    add pull request opened and closed handler

--- index.js
+++ index.js
@@ -2,18 +2,62 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
-module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
 
-  app.on('pull_request.opened', async context => {
-    const pullComment = context.pull({ body: 'Thanks for submitting this Pull Request!' })
-    return context.github.pulls.createComment(pullComment)
+module.exports = app => {
+
+  app.on(['pull_request.opened','pull_request.reopened'], async context => {
+    const config = (await context.config('auto_pr_team.yml'))
+
+    if (!config) {
+      throw new Error('the configuration file failed to load')
+    }
+  
+    const {
+      org,
+      team,
+    } = config
+    const owner = context.payload.pull_request.user.login
+    const teamsRequest = await context.github.teams.list({ org: config.org})
+
+    for (const team of teamsRequest.data) {
+      if (team.name == config.team) {
+        console.log(`Found team:`, JSON.stringify(team))
+        const teamsRequest = await context.github.teams.addOrUpdateMembershipInOrg({
+          org: config.org,
+          team_slug: team.name,
+          username: owner,
+        });
+        break;
+      }
+    }
+    return
   })
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  app.on('pull_request.closed', async context => {
+    const config = (await context.config('auto_pr_team.yml'))
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    if (!config) {
+      throw new Error('the configuration file failed to load')
+    }
+  
+    const {
+      org,
+      team,
+    } = config
+    const owner = context.payload.pull_request.user.login
+    const teamsRequest = await context.github.teams.list({ org: config.org})
+
+    for (const team of teamsRequest.data) {
+      if (team.name == config.team) {
+        console.log(`Found team:`, JSON.stringify(team))
+        const teamsRequest = await context.github.teams.removeMembershipInOrg({
+          org: config.org,
+          team_slug: team.name,
+          username: owner,
+        });
+        break;
+      }
+    }
+    return
+  })
 }
